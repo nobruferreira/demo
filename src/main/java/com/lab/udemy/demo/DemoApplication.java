dda8e0112d727a940cdc179f969b4f7c1ce88050
nobruferreira <bruferreira.1994@gmail.com>
2018-09-08 12:33:22 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/lab/udemy/demo/DemoApplication.java
+++ src/main/java/com/lab/udemy/demo/DemoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lab.udemy.demo.domain.Categoria;
+import com.lab.udemy.demo.domain.Produto;
 import com.lab.udemy.demo.repositories.CategoriaRepository;
+import com.lab.udemy.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -26,6 +30,18 @@ public class DemoApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 }
