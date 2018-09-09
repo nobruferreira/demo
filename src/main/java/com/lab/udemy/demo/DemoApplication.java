43a883f540bd43577e5fadaa35490ac3d0633965
nobruferreira <bruferreira.1994@gmail.com>
2018-09-08 23:14:32 -0300

    Estado e Cidade

--- src/main/java/com/lab/udemy/demo/DemoApplication.java
+++ src/main/java/com/lab/udemy/demo/DemoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lab.udemy.demo.domain.Categoria;
+import com.lab.udemy.demo.domain.Cidade;
+import com.lab.udemy.demo.domain.Estado;
 import com.lab.udemy.demo.domain.Produto;
 import com.lab.udemy.demo.repositories.CategoriaRepository;
+import com.lab.udemy.demo.repositories.CidadeRepository;
+import com.lab.udemy.demo.repositories.EstadoRepository;
 import com.lab.udemy.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class DemoApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -43,5 +51,18 @@ public class DemoApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 	}
 }
